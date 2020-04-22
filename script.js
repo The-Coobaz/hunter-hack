c2d4a480147f42cb2303996163cdf5c4cf99d275
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-04-22 16:31:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-   $(document).ready(function(){
-        $("input[type='button']").click(function(){
-            var radioValue = $("input[name='Section']:checked").val();
-            if(radioValue){
-                alert("Your are a - " + radioValue);
-            }
-        });
-    });
+$(document).ready(function () {
+    "use strict";
+    var $programNo;
+    var $timeval;
+    var $secnum;
+    var $killButton = $('#kill-button');
+   
+$waterSection($secnum, $timeval)(); {
+    $secnum = ("input[name='$secnum']:checked").val();
+    $timeval = ("input[name='$timeval']:checked").val();
+   if ($secnum && $timeval)  { alert("Watering "+$secnum+" section for "+$timeval+" minutes")}}
+  
+});
