597151548bdb0cda5f9ecad96cb695a81f4223a7
Wiiitek <kubiczak.jakub@gmail.com>
2020-04-22 18:48:22 +0200

    revert rename for easiercode review

--- buttons.js
+++ buttons.js
@@ -1,6 +1,6 @@
-function waterSection(sectionValue, timeValue) {
+function waterSection(radioValue, timeValue) {
     'use strict';
-    var b = sectionValue * timeValue;
+    var b = radioValue * timeValue;
     return b;
     //here we start watering
 }
@@ -14,22 +14,22 @@ $(document).ready(function () {
     'use strict';
     
     //we add click listeners on buttons specified by id
-    $('#startProgram').click(function(){
+    $('#startProgram').click(function () {
         var programNo = $("input[name='programNo']:checked").val();
         if (programNo) {startProgram(programNo); }
     });
 
-    $('#waterSection').click(function(){
-        var sectionValue = $("input[name='secnum']:checked").val(),
+    $('#waterSection').click(function () {
+        var radioValue = $("input[name='secnum']:checked").val(),
             timeValue = $("input[name='timeval']:checked").val();
 
-        if (sectionValue && timeValue) {
-            alert("Selected section number " + sectionValue + "selected time:" + timeValue);
-            waterSection(sectionValue, timeValue);
+        if (radioValue && timeValue) {
+            alert("Selected section number " + radioValue + "selected time:" + timeValue);
+            waterSection(radioValue, timeValue);
         }
     });
 
-    $('#kill-button').click(function(){
+    $('#kill-button').click(function () {
         alert("Stopping all");
     });
 });
